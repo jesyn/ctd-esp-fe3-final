a4405201319f7b6dc18f9b539ee9922af18480a9
Jesica Muñoz <jesydynathos@gmail.com>
2023-09-05 18:03:46 -0300

    theme material: cambios en la paleta de colores

--- styles/material-theme.ts
+++ styles/material-theme.ts
@@ -1,7 +1,20 @@
-import {createTheme} from "@mui/material";
+import { createTheme } from "@mui/material";
 
 export const theme = createTheme({
-    typography: {
-        fontFamily: 'Roboto, sans-serif'
+  typography: {
+    fontFamily: "Roboto, sans-serif",
+  },
+  palette: {
+    primary: {
+      main: "#629AD4", //blue
     },
+    secondary: {
+      main: "#EBA74D", // orange
+    },
+  },
 });
+
+/* red: "#DF5D3A",
+darkGrey: "#BBBBBB",
+black: "#302B2D",
+lightGrey: "#E8E8E8", */
