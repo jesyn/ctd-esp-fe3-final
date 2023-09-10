8e23dcebcdc20f37091566abeb3b92bf653e4047
Jesica Muñoz <jesydynathos@gmail.com>
2023-09-10 02:51:52 -0300

    feat: input custom

---
+++ components/InputCustom/InputCustom.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import TextField from "@mui/material/TextField";
+import { Control, Controller } from "react-hook-form";
+
+interface CustomTextFieldProps {
+  name: string;
+  label: string;
+  type: string;
+  required?: boolean;
+  control: Control<any>;
+  placeholder?: string;
+  defaultValue?: string;
+  error?: boolean;
+  messageError?: string;
+}
+
+const InputCustom = ({
+  name,
+  label,
+  type,
+  required,
+  control,
+  placeholder,
+  defaultValue,
+  error,
+  messageError,
+}: CustomTextFieldProps) => {
+  return (
+    <Controller
+      name={name}
+      control={control}
+      defaultValue={defaultValue}
+      render={({ field }) => (
+        <TextField
+          {...field}
+          type={type}
+          label={label}
+          variant="outlined"
+          fullWidth
+          placeholder={placeholder}
+          required={required}
+          error={error}
+          helperText={messageError}
+          sx={{ mb: 2 }}
+        />
+      )}
+    />
+  );
+};
+
+export default InputCustom;
