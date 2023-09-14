49f72e68a1ec61d4a4acb199e612133dc97cd9de
Jesica Muñoz <jesydynathos@gmail.com>
2023-09-14 02:13:07 -0300

    changing middleware for cookie library

--- middleware.page.ts
+++ middleware.page.ts
@@ -1,13 +1,15 @@
+//Se intento proteger la ruta de "confirmacion-compra" a través del middleware pero al final se resolvio seteando una
+//cokkie con la libreria "js-cookie"
+
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(req: NextRequest) {
   const access = req.cookies.get("Access");
   const url = req.nextUrl.pathname;
 
-  if (url.includes("confirmacion-compra") && !access) {
-    console.log("entro");
+  /* if (url.includes("confirmacion-compra") && !access) {
     return NextResponse.redirect(new URL("/", req.url));
-  }
+  } */
 }
 
 export const config = {
