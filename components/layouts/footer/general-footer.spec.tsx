1b816f3c60ee4ca4d85e73f66112a8f62655d9f4
Jesica Muñoz <jesydynathos@gmail.com>
2023-09-14 02:18:23 -0300

    fix: footer test

--- components/layouts/footer/general-footer.spec.tsx
+++ components/layouts/footer/general-footer.spec.tsx
@@ -1,17 +1,17 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen } from "@testing-library/react";
 import GeneralFooter from "dh-marvel/components/layouts/footer/general-footer.component";
 
-describe('GeneralFooter', () => {
-    describe('when rendering default layout', () => {
-        it('should render the powered by text', () => {
-            render(<GeneralFooter />)
-            const poweredBy = screen.getByText('Powered by')
-            expect(poweredBy).toBeInTheDocument()
-        })
-        it('should render the logo', () => {
-            render(<GeneralFooter />)
-            const logo = screen.getByAltText('Digital House Logo')
-            expect(logo).toBeInTheDocument()
-        })
-    })
-})
+describe("GeneralFooter", () => {
+  describe("when rendering default layout", () => {
+    it("should render the powered by text", () => {
+      render(<GeneralFooter />);
+      const poweredBy = screen.getByText("Powered by");
+      expect(poweredBy).toBeInTheDocument();
+    });
+    it("should render the logo", () => {
+      render(<GeneralFooter />);
+      const logo = screen.getByAltText("Logo jesy");
+      expect(logo).toBeInTheDocument();
+    });
+  });
+});
