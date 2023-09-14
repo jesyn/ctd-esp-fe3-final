edd775736d6d37c8e5149cd47080ca7a2cf26879
Jesica Muñoz <jesydynathos@gmail.com>
2023-09-14 02:17:36 -0300

    set cookie

--- pages/confirmacion-compra/index.page.tsx
+++ pages/confirmacion-compra/index.page.tsx
@@ -1,14 +1,15 @@
 import { Container } from "@mui/material";
 import PurchaseConfirmation from "dh-marvel/components/PurchaseConfirmation/PurchaseConfirmation";
 import LayoutCheckout from "dh-marvel/components/layouts/layout-checkout";
-import LayoutGeneral from "dh-marvel/components/layouts/layout-general";
 import { useRouter } from "next/router";
 import React from "react";
+import Cookies from "js-cookie";
 
 const ConfirmationPage = () => {
   const router = useRouter();
 
   const handleClickGoHome = () => {
+    Cookies.remove("access", { path: "/confirmacion-compra" });
     router.push("/");
   };
 
